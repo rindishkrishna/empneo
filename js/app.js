d47a2afe744719df4df4a03b8b3cfed098b14da0
Afnan Navaz <42640846+Afnan-Navaz@users.noreply.github.com>
2019-05-22 01:36:13 +0530

    confused (#8)
    * map
    
    * Add files via upload
    
    * Update style.css
    
    * Add files via upload
    
    * Delete menu.png
    
    * Delete close.png
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,9 +3,19 @@ let but = document.querySelector('#menu-but');
 but.addEventListener('click', (e)=>{
     if(document.querySelector('#drop').style.display == 'none'){
         document.querySelector('#drop').style.display = 'block';
+        document.querySelector('#men').src = 'images/close.png';
     }else{
         document.querySelector('#drop').style.display = 'none'
+        document.querySelector('#men').src = 'images/menu.png';
     }
     e.preventDefault();
 });
 
+window.onscroll = function(){
+    if(this.oldScroll < this.scrollY && window.innerWidth <= 700){
+        document.querySelector('.navbar').style.top = '10px';
+    }else{
+        document.querySelector('.navbar').style.top = '0px';
+    }
+    this.oldScroll = this.scrollY;
+}
